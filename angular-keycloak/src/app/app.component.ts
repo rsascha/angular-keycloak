eca4ed252e8292d84b5e6b0337d54960d9e6acde
rsascha <sascha.rose@gmail.com>
2020-09-29 17:12:19 +0200

    Add test output

--- angular-keycloak/src/app/app.component.ts
+++ angular-keycloak/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { KeycloakService } from './keycloak.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-keycloak';
+
+  constructor(private keycloak: KeycloakService) {}
+
+  get loginNameTest$(): Observable<string> {
+    return of('Test User');
+  }
+
+  get loginName$(): Observable<string> {
+    return this.keycloak.getUserName();
+  }
 }
