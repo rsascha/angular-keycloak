08c688907e9a90fa7308b2e805aede0ee6a67e17
Sascha Rose <sascha.rose@gmail.com>
2020-10-17 09:49:58 +0200

    Setup API routes

--- application-service/src/app.controller.ts
+++ application-service/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app-controller')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get('get-hello')
+    getHello(): string {
+        return this.appService.getHello();
+    }
 }
--- application-service/src/main.ts
+++ application-service/src/main.ts
@@ -5,20 +5,22 @@ import { AppModule } from './app.module';
 import environment from './environment';
 
 async function bootstrap() {
-  const port = environment()['port'];
-  const logger = new Logger('bootstrap()');
-  const app = await NestFactory.create(AppModule);
+    const port = environment()['port'];
+    const logger = new Logger('bootstrap()');
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('application-service/v1');
 
-  const options = new DocumentBuilder()
-    .setTitle('Application Service')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+    const options = new DocumentBuilder()
+        .setTitle('Application Service')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(port);
-  app.getUrl().then(url => {
-    logger.log(`Listening on: ${url}`);
-  });
+    await app.listen(port);
+    app.getUrl().then(url => {
+        logger.log(`Service is listening on: ${url}`);
+        logger.log(`Swagger is running on: ${url}/swagger`);
+    });
 }
 bootstrap();
