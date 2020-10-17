e052a142a7fc5993b7dae5373f0c198e2390749f
Sascha Rose <sascha.rose@gmail.com>
2020-10-17 09:40:52 +0200

    Added swagger, logging, configuration

--- application-service/src/app.module.ts
+++ application-service/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import environment from './environment';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [environment],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- application-service/src/main.ts
+++ application-service/src/main.ts
@@ -1,8 +1,24 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import environment from './environment';
 
 async function bootstrap() {
+  const port = environment()['port'];
+  const logger = new Logger('bootstrap()');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('Application Service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  app.getUrl().then(url => {
+    logger.log(`Listening on: ${url}`);
+  });
 }
 bootstrap();
