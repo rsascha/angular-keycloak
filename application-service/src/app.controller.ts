ee3537a8b15bfb033ae736d97c21a1e59ea1d4f6
Sascha Rose <sascha.rose@gmail.com>
2020-10-17 11:33:39 +0200

    Add dummy data model

--- application-service/src/app.controller.ts
+++ application-service/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { AppService } from './app.service';
+
+export class HelloResponse {
+    @ApiProperty({ type: String })
+    data: string;
+}
 
 @Controller('app-controller')
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get('get-hello')
-    getHello(): string {
-        return this.appService.getHello();
+    @ApiTags('get-hello')
+    @ApiResponse({ status: 200, type: HelloResponse })
+    getHello(): HelloResponse {
+        const apiResponse = {
+            data: this.appService.getHello(),
+        } as HelloResponse;
+        return apiResponse;
     }
 }
