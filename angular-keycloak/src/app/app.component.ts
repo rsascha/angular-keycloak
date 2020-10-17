9b6e48a8fc29099192a8459fb625f0630cc070fb
Sascha Rose <sascha.rose@gmail.com>
2020-10-17 08:54:13 +0200

    Remove Test Code

--- angular-keycloak/src/app/app.component.ts
+++ angular-keycloak/src/app/app.component.ts
@@ -1,21 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { KeycloakService } from './keycloak.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { Observable } from 'rxjs'
+import { KeycloakService } from './keycloak.service'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'angular-keycloak';
-  loginName$: Observable<string>;
-  constructor(private keycloak: KeycloakService) {
-    this.loginName$ = this.keycloak.getUserName();
-  }
-
-  get loginNameTest$(): Observable<string> {
-    return of('Test User');
-  }
+    title = 'angular-keycloak'
+    loginName$: Observable<string>
+    constructor(private keycloak: KeycloakService) {
+        this.loginName$ = this.keycloak.getUserName()
+    }
 }
